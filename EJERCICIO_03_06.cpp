912ed37798a82de5cfe0c537ddaa19a7e9331053
Leonardo Delgado Medrano <leonardo.delgado@ucb.edu.bo>
2023-09-06 23:51:43 -0400

    Corrección ejercicio 6

--- EJERCICIO_03_06.cpp
+++ EJERCICIO_03_06.cpp
@@ -2,25 +2,25 @@
 // Autor: Leonardo Delgado Medrano
 // Fecha creacion: 06/09/2023
 // Fecha modificacion: 06/09/2023
-// Numero de ejericio: 5
+// Numero de ejericio: 6
 // Problema planteado: Generar la matriz para un orden NxN
 #include<iostream>
-#include<random>
+#include<iomanip>
 using namespace std;
 int main()
 {
-    int orden,valorA,valorB;
+    int orden,valorA,valorB, secuencia1=0,secuencia2=1,secuencia3;
     cout<<"Ingrese el orden de la matriz\n";cin>>orden;
-    cout<<"Ingrese los limites de los valores para la matriz\n";cin>>valorA>>valorB;
     int Matriz[orden][orden];
-    if(valorA>valorB)
-        swap(valorA,valorB);//Cambia los valores de valorA y valorB
     for(int i=0;i<orden;i++)
     {
         for(int j=0;j<orden;j++)
         {
-            Matriz[i][j]=rand() %(valorB-valorA+1)+valorA;
-            cout<<Matriz[i][j]<<" ";
+            Matriz[i][j]=secuencia2;
+            secuencia3=secuencia2+secuencia1;
+            secuencia1=secuencia2;
+            secuencia2=secuencia3;
+            cout<<Matriz[i][j]<<setw(12);
         }
         cout<<"\n";
     }
